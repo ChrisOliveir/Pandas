04c894f98750825e518e7b337df49bf69cfd13be
christiane oliveira <christianeolive@hotmail.com>
2022-09-20 17:12:50 -0300

    estudando pandas

---
+++ Aula1.py
@@ -0,0 +1,36 @@
+﻿from sys import displayhook
+from tkinter.tix import DisplayStyle
+import pandas as pd 
+
+# lendo arquivo CSV 
+# as vezes precisaremos mudar o encoding, possiveis valores para testar:
+# encoding='Latin1', encoding='ISO-8859-1',' encoding='utf-8' ou encoding='cp1252'
+
+vendas_df = pd.read_csv('Vendas.csv', sep=";")
+produtos_df = pd.read_csv('Cadastro Produtos.csv', sep=";")
+lojas_df = pd.read_csv('Lojas.csv', sep=";")
+clientes_df = pd.read_csv('Clientes.csv', sep=";")
+print(vendas_df.info()) 
+ 
+ #escolhendo colunas que quero pegar
+clientes_df = clientes_df[['ID Cliente', 'E-mail']]
+produtos_df = produtos_df[['ID Produto', 'Nome do Produto']]
+lojas_df = lojas_df[['ID Loja', 'Nome da Loja']]
+
+#juntando os dataframes 
+vendas_df = vendas_df.merge(produtos_df, on='ID Produto')
+vendas_df = vendas_df.merge(lojas_df, on='ID Loja')
+vendas_df = vendas_df.merge(clientes_df, on="ID Cliente")
+
+
+
+# renomeando o nome da coluna e-mail 
+vendas_df = vendas_df.rename(columns={'E-mail': 'E-mail do Cliente'})
+
+#exibindo o dataframe final 
+displayhook(vendas_df)
+
+#exibindo as tabelas 
+#displayhook(produtos_df)
+#displayhook(lojas_df)
+#displayhook(clientes_df)
---
+++ proj.py
@@ -0,0 +1,25 @@
+﻿import pyodbc 
+import numpy as np
+import pandas as pd 
+
+
+dados_conexao = ("Driver={SQLite3 ODBC Driver}; Server=localhost; Database=chinook.db")
+
+# caso precise de login e senha :
+#dados_conexão = ("Driver={meu drive}; Server= meuservidor; Database=NomeBaseDeDados;
+# UID= Login; PWD= Senha;")
+
+conexao = pyodbc.connect(dados_conexao)
+print("ok")
+
+cursor = conexao.cursor() # o cursor é o cara que comanda a integração do banco com o pyobdc
+
+cursor.execute("SELECT * FROM customers")  # leia todas as informações da tabela Customers
+
+valores = cursor.fetchall() # esta armazenando as iformações dentro da variavel valor no formato de uma lista de tuplas
+# print(valores[0]) # primeiro 10 valores
+
+cursor.close()
+tabela_clientes = pd.DataFrame.from_records(valores)
+
+
